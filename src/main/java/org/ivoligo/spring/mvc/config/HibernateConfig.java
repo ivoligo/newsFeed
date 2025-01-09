79578ab75a9fd3e98cd3b2fe176ae4cbca5fb887
OLIvanov <OLIvanov@systematic.ru>
2025-01-09 18:56:40 +0300

    fix outline add dataSource

--- src/main/java/org/ivoligo/spring/mvc/config/HibernateConfig.java
+++ src/main/java/org/ivoligo/spring/mvc/config/HibernateConfig.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
@@ -16,6 +19,16 @@ public class HibernateConfig {
 
     @Autowired
     private Environment env;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getRequiredProperty("jdbc.driverClassName"));
+        dataSource.setUrl(env.getRequiredProperty("jdbc.url"));
+        dataSource.setUsername(env.getRequiredProperty("jdbc.username"));
+        dataSource.setPassword(env.getRequiredProperty("jdbc.password"));
+        return dataSource;
+    }
 
     @Bean
     public LocalSessionFactoryBean getSessionFactory() {
